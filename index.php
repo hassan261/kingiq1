32e697229aaf346a5a35d121b718a95549d9ed4a
hassan261 <uuqq111112@gmail.com>
2016-09-15 04:19:15 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>iraqmoves.com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-rtl.css">
+  <link rel="stylesheet" type="text/css" href="css/websit.css">
+  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+<div class="container-fluid">
+<nav class="navbar navbar-inverse  " style="color:red;	 font-size: 17px; background-color:#003333;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" >
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">مشاهده الافلام</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">الرئيسيه</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#"> انواع الافلام <span class="caret"></span></a>
+          <ul class="dropdown-menu" >
+            <li><a href="#" style="color:#000 bold; font-size:17px;">الافلام الاجنبيه</a></li>
+            <li><a href="#" style="color:#000; font-size:17px;">الافلام العربيه</a></li>
+            <li><a href="#" style="color:#000; font-size:17px;">الافلام المتنوعه </a></li>
+          </ul>
+        </li>
+        <li><a href="#">كوميدي</a></li>
+        <li><a href="#">حول الموقع</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+<a href="" ><span class="fa fa-facebook-f" style="font-size:26px ; float:left; margin-left:30px;margin-top: 14px; color: white ;"></span></a>
+<a href="" ><span class="fa fa-google-plus" style="font-size:26px ; float:left; margin-left:30px;margin-top: 14px;color: white ;"></span></a>
+<a href="" ><span class="	 fa fa-twitter" style="font-size:26px ; float:left; margin-left:30px;margin-top: 14px;color: white ;"></span></a>
+
+    </div>
+  </div>
+</nav>
+
+<section class="film">
+	<header class="titele"><P>فلم im legend</P></header>
+	<img src="image/b9">
+    <footer class="ahov"><p>فلم الزومبي الرائع والجميل 2014</p> <a href="#" style="color:#fff;">تحميل-- <span class="glyphicon glyphicon-log-in"> </span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>The Expendple</P></header>
+	<img src="image/b8">
+    <footer class="ahov"><p>فلم الاكشن والمغامره الرائع 2016 حصريا</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>Sonw White</P></header>
+	<img src="image/b1">
+    <footer class="ahov"><p>فلم المغامره والفنتازيا 2015</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>sarkar Raj</P></header>
+	<img src="image/b2">
+    <footer class="ahov"><p>الفلم الهندي المنضر 2016</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>Bait</P></header>
+	<img src="image/b3">
+    <footer class="ahov"><p>فلم المغامره والاثاره 2016</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>Avengrs</P></header>
+	<img src="image/b4">
+    <footer class="ahov"><p>لم الاكشن المنضر 2016</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>Bolet To The Head</P></header>
+	<img src="image/b5">
+    <footer class="ahov"><p>اجمل افلام سلفستر ستالون 2017</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>The Day After Twomro</P></header>
+	<img src="image/b6">
+    <footer class="ahov"><p>فلم الاكشن والحروب الرائع 2016</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>The Expendble</P></header>
+	<img src="image/b7">
+    <footer class="ahov"><p>فلم الزومبي الرائع والجميل</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P>Venom</P></header>
+	<img src="image/b12">
+    <footer class="ahov"><p>فلم الرعب الرائع +18 2016</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+<section class="film">
+	<header class="titele"><P> Etorn of the datd</P></header>
+	<img src="image/b11">
+    <footer class="ahov"><p>فلم الرعب والفنتازيا +18</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+
+<section class="film">
+	<header class="titele"><P>Avengers 2</P></header>
+	<img src="image/b10">
+    <footer class="ahov"><p>الابطال الخارقون المنتضر 2016</p> <a href="#" style="color:#fff;">تحميل--<span class="glyphicon glyphicon-log-in"></span></a></footer>
+</section>
+
+</div>
+
+</body>
+</html>
